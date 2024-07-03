1b85e9b72166ee25bc0a2e93fcd93c46d2a9ce96
ghostjidihd <ghostjidihd@gmail.com>
2024-07-03 07:46:58 +0000

    Added heart sprite

---
+++ lib/game/overlays/score_display.dart
@@ -0,0 +1,25 @@
+import 'package:flame/components.dart';
+import 'package:flame/text.dart';
+import 'package:flutter/material.dart';
+
+class ScoreDisplay extends TextComponent {
+  int score;
+
+  ScoreDisplay({required this.score})
+      : super(
+          text: 'SCORE: $score',
+          textRenderer: TextPaint(
+            style: TextStyle(
+              fontSize: 24,
+              fontFamily: 'CosmicAlien',
+              color: Colors.white,
+            ),
+          ),
+        );
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    text = 'SCORE: $score';
+  }
+}
