847e9979bbff71f7099691b5effeffd3a445810d
ghostjidihd <ghostjidihd@gmail.com>
2024-07-04 12:20:57 +0000

    Added app icon

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -19,68 +19,84 @@ class MainMenu extends StatelessWidget {
       body: Container(
         color: const Color(0xFF121212), // Very dark gray background color
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(height: 40),
-              const Text(
-                'Galactic Invaders',
-                style: TextStyle(
-                  fontFamily: 'ElectronPulse',
-                  fontSize: 50,
-                  color: Colors.greenAccent,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 10.0,
-                      color: Colors.black,
-                      offset: Offset(3.0, 3.0),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(height: 40),
+                  const Text(
+                    'Galactic Invaders',
+                    style: TextStyle(
+                      fontFamily: 'ElectronPulse',
+                      fontSize: 50,
+                      color: Colors.greenAccent,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black,
+                          offset: Offset(3.0, 3.0),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 250, // Fixed width for the buttons
-                child: OutlinedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const GamePage()),
-                    );
-                  },
-                  style: OutlinedButton.styleFrom(
-                    textStyle: const TextStyle(fontFamily: 'CosmicAlien', fontSize: 20),
-                    side: const BorderSide(color: Colors.green, width: 2), // Button outline color
-                    foregroundColor: Colors.green, // Button text color
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: 250, // Fixed width for the buttons
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const GamePage()),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        textStyle: const TextStyle(fontFamily: 'CosmicAlien', fontSize: 20),
+                        side: const BorderSide(color: Colors.green, width: 2), // Button outline color
+                        foregroundColor: Colors.green, // Button text color
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text('Spiel starten'),
                     ),
                   ),
-                  child: const Text('Spiel starten'),
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 250, // Fixed width for the buttons
-                child: OutlinedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ScoreboardPage()),
-                    );
-                  },
-                  style: OutlinedButton.styleFrom(
-                    textStyle: const TextStyle(fontFamily: 'CosmicAlien', fontSize: 20),
-                    side: const BorderSide(color: Colors.green, width: 2), // Button outline color
-                    foregroundColor: Colors.green, // Button text color
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 250, // Fixed width for the buttons
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ScoreboardPage()),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        textStyle: const TextStyle(fontFamily: 'CosmicAlien', fontSize: 20),
+                        side: const BorderSide(color: Colors.green, width: 2), // Button outline color
+                        foregroundColor: Colors.green, // Button text color
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text('Scoreboard'),
                     ),
                   ),
-                  child: const Text('Scoreboard'),
+                ],
+              ),
+              Positioned(
+                top: 100,
+                right: 35,
+                child: Transform.rotate(
+                  angle: 0.55, // Around 31 degrees in radian
+                  child: Image.asset(
+                    'assets/images/sprites/enemy.png',
+                    width: 70,
+                    height: 70,
+                  ),
                 ),
               ),
             ],
